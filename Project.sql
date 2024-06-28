6246a00116fd2ebe2db5bff28d309f4fc09daba7
karthik12 <karthik12>
2024-06-28 10:26:50 +0530

    first program sql

---
+++ Project.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE item;
+USE item;
+CREATE TABLE pen(id int,pen_name varchar(20),pen_price int);
+
+CREATE TABLE job(id int,job_name varchar(20),job_salary bigint,job_place varchar(20));
+
+CREATE TABLE teacher(id int,teacher_name varchar(20),teacher_salary bigint,teacher_place varchar(20));
+
+CREATE DATABASE project;
+USE project;
+Create table Airport_details(id int,airport_name varchar(20),arrival int,departure int,price bigint);
+
+Create table Cricket_teams(id int,team_name varchar(20),no_of_players int,captain varchar(20),coach varchar(20));
